3b5e3309cc3721a5539ea8ec24d464f6cdb7dd82
plcstevens <philip@tauri-tec.com>
2012-12-20 17:31:02 +0000

    Added in some configurations that'll be needed when the server starts, like base api url, username etc that the user should be allowed to set.
    We still need to work out how to read the local environment if they are not present.

--- lib/comufyrails.rb
+++ lib/comufyrails.rb
@@ -4,5 +4,13 @@ require "comufyrails/version"
 require 'comufyrails/railtie' if defined?(Rails)
 
 module Comufyrails
-  # Your code goes here...
+
+  class Config
+    attr_accessor :app_name, :username, :password, :access_token, :expiry_time, :base_api_url
+  end
+
+  def self.config
+    @@config ||= Config.new
+  end
+
 end
--- lib/comufyrails/railtie.rb
+++ lib/comufyrails/railtie.rb
@@ -1,25 +1,61 @@
-require 'comufyrails'
 require 'rails'
 
-module Comufyrails
-  class Railtie < Rails::Railtie
+class Comufyrails::Railtie < Rails::Railtie
 
-    config.comufy = ActiveSupport::OrderedOptions.new
+  # this allows users to manage settings just like they manage rails settings
+  config.comufy = ActiveSupport::OrderedOptions.new
 
-    rake_tasks do
-      # load "path/to/my_railtie.tasks"
-    end
+  rake_tasks do
+    # load "path/to/my_railtie.tasks"
+  end
 
-    initializer "comufyrails.configure_settings" do |app|
-      unless defined?(COMUFY)
-        silence_warnings { Object.const_set "COMUFY", app.config.comufy }
-      end
-    end
-
-    # Add a to_prepare block which is executed once in production
-    # and before each request in development
-    config.to_prepare do
-      # something
+  initializer "comufyrails.configure_settings" do |app|
+    #unless defined?(COMUFY)
+    #  silence_warnings { Object.const_set "COMUFY", app.config.comufy }
+    #end
+    Comufyrails.configure do |config|
+      config.app_name     = app.config.comufy.delete(:app_name)     || self.app_name
+      config.username     = app.config.comufy.delete(:username)     || self.username
+      config.password     = app.config.comufy.delete(:password)     || self.password
+      config.access_token = app.config.comufy.delete(:access_token) || self.access_token
+      config.expiry_date  = app.config.comufy.delete(:expiry_date)  || self.expiry_date
+      config.base_api_url = app.config.comufy.delete(:base_api_url) || self.base_api_url
     end
   end
+
+  # Add a to_prepare block which is executed once in production
+  # and before each request in development
+  config.to_prepare do
+    # something
+  end
+
+  private
+
+    def self.app_name
+      ::Rails.application.class.to_s.split("::").first
+    end
+
+    def self.username
+      # TODO: look in the environment for comufy settings before returning nil
+
+    end
+
+    def self.password
+      # TODO: look in the environment for comufy settings before returning nil
+
+    end
+
+    def self.access_token
+      # TODO: look in the environment for comufy settings before returning nil
+
+    end
+
+    def self.expiry_token
+      # TODO: look in the environment for comufy settings before returning nil
+
+    end
+
+    def self.base_api_url
+      'http://www.sociableapi.com/xcoreweb/client'
+    end
 end
