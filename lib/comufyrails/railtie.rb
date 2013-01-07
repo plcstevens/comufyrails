f93fd97a9617b691a9ccfb4160469ac51e218950
plcstevens <philip@tauri-tec.com>
2013-01-07 15:49:07 +0000

    Removed some additional method names

--- lib/comufyrails/railtie.rb
+++ lib/comufyrails/railtie.rb
@@ -12,13 +12,13 @@ class Comufyrails::Railtie < Rails::Railtie
   end
 
   initializer "comufyrails.configure" do |app|
-    Comufyrails.configure do |config|
-      config.app_name     = app.config.comufy_rails[:app_name]      || Comufyrails::Railtie.app_name
-      config.access_token = app.config.comufy_rails[:access_token]  || Comufyrails::Railtie.access_token
-      config.base_api_url = app.config.comufy_rails[:base_api_url]  || Comufyrails::Railtie.base_api_url
+    Comufyrails.configure do |c|
+      c.app_name     = app.config.comufy_rails[:app_name]     || ENV.fetch('COMUFY_APP_NAME',     ::Rails.application.class.to_s.split("::").first)
+      c.access_token = app.config.comufy_rails[:access_token] || ENV.fetch('COMUFY_TOKEN',        nil)
+      c.base_api_url = app.config.comufy_rails[:base_api_url] || ENV.fetch('COMUFY_BASE_API_URL', 'http://www.sociableapi.com/xcoreweb/client')
 
       # we just want a date far into the future
-      config.expiry_time  = Time.now.to_i + 1000000
+      c.expiry_time  = Time.now.to_i + 1000000
     end
   end
 
@@ -26,24 +26,4 @@ class Comufyrails::Railtie < Rails::Railtie
   rake_tasks do
     load "tasks/comufyrails.rake"
   end
-
-  # Add a to_prepare block which is executed once in production
-  # and before each request in development
-  config.to_prepare do
-    # something
-  end
-
-  private
-
-    def self.app_name
-      ENV.fetch('COMUFY_APP_NAME', ::Rails.application.class.to_s.split("::").first)
-    end
-
-    def self.access_token
-      ENV.fetch('COMUFY_TOKEN', nil)
-    end
-
-    def self.base_api_url
-      ENV.fetch('COMUFY_BASE_API_URL', 'http://www.sociableapi.com/xcoreweb/client')
-    end
 end
